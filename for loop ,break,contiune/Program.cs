af4e484622cd570458184462737963d725ee9466
mehmetterdem <erdemmimet@gmail.com>
2022-02-24 09:41:29 +0300

    Add project files.

---
+++ for loop ,break,contiune/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace for_loop__break_contiune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sayı giriniz");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <=sayac; i++)
+            {
+                if (i%2==1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            int tektoplam = 0;
+            int cifttoplam = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tektoplam += i;
+                }
+                else
+                    cifttoplam += i;
+            }
+            Console.WriteLine("tektoplam: "+tektoplam+"\ncifttoplam: "+cifttoplam);
+
+            //break,contiune
+            for (int i = 0; i < 10; i++)
+            {
+                if (i==4)
+                {
+                    continue;
+
+                }
+             Console.WriteLine(i);
+            }
+           
+            
+
+
+
+        }
+    }
+}
