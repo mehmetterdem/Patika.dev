9892c807c5c5c31c8027ea7650582110f875b4f6
mehmetterdem <erdemmimet@gmail.com>
2022-02-26 12:52:54 +0300

    array ve metotları

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sıralama
+            int[] sayıdizisi = { 23, 12, 4, 72, 3, 11, 17 };
+            foreach (var item in sayıdizisi)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("sıralı dizi");
+            Array.Sort(sayıdizisi);
+            foreach (var item in sayıdizisi)
+            {
+                Console.WriteLine(item);
+            }
+            //clear
+            Console.WriteLine("array clear");
+            Array.Clear(sayıdizisi, 2, 2);
+            foreach (var item in sayıdizisi)
+            {
+                Console.WriteLine(item);
+            }
+            //reverse,
+            Console.WriteLine("array reverse");
+            Array.Reverse(sayıdizisi);
+            foreach (var item in sayıdizisi) 
+            {
+                Console.WriteLine(item);
+            }
+
+            //indexOf
+            Console.WriteLine("arrayindexOf");
+            Console.WriteLine(Array.IndexOf(sayıdizisi,23));
+
+            //resize
+
+            Console.WriteLine("array size");
+            Array.Resize<int>(ref sayıdizisi, 9);
+            sayıdizisi[8] = 99;
+            foreach (var item in sayıdizisi)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
