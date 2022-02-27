ea5c63793ae31ca13580964357eeb18827661224
mehmetterdem <erdemmimet@gmail.com>
2022-02-27 17:48:33 +0300

    hazır metotlar datetime ve math

--- HazırMetodlar-1-string/Program.cs
+++ HazırMetodlar-1-string/Program.cs
@@ -7,93 +7,97 @@ namespace HazırMetodlar_1_string
         static void Main(string[] args)
         {
 
-            string degisken = "Dersimiz cSharp,Hosgeldiniz";
+            string degisken = "Dersimiz6 cSharp,Hosgeldi5niz";
             string degisken2 = " cSharp";
-            //length:
-            Console.WriteLine(degisken.Length);
+            ////length:
+            //Console.WriteLine(degisken.Length);
 
-            //toUpper  ToLower
+            ////toUpper  ToLower
 
-            Console.WriteLine(degisken.ToLower());
-            Console.WriteLine(degisken.ToUpper());
+            //Console.WriteLine(degisken.ToLower());
+            //Console.WriteLine(degisken.ToUpper());
 
-            //concat
+            ////concat
 
-            Console.WriteLine(string.Concat(degisken,"merhaba"));
+            //Console.WriteLine(string.Concat(degisken,"merhaba"));
 
-            //compare-compareto
+            ////compare-compareto
 
-            Console.WriteLine(degisken.CompareTo(degisken2));//0,1,-1
-            Console.WriteLine(string.Compare(degisken,degisken2,true));
-            Console.WriteLine(string.Compare(degisken, degisken2,false));
-            //contains
+            //Console.WriteLine(degisken.CompareTo(degisken2));//0,1,-1
+            //Console.WriteLine(string.Compare(degisken,degisken2,true));
+            //Console.WriteLine(string.Compare(degisken, degisken2,false));
+            ////contains
 
-            Console.WriteLine(degisken.Contains(degisken2));
-            Console.WriteLine(degisken.StartsWith("Hosgeldiniz"));
-            Console.WriteLine(degisken.EndsWith("Hosgeldiniz"));
+            //Console.WriteLine(degisken.Contains(degisken2));
+            //Console.WriteLine(degisken.StartsWith("Hosgeldiniz"));
+            //Console.WriteLine(degisken.EndsWith("Hosgeldiniz"));
 
-            //indexof
-            Console.WriteLine(degisken.IndexOf("cS"));
-            Console.WriteLine(degisken.IndexOf("mehmet"));
-            Console.WriteLine(degisken.LastIndexOf("i"));
-            //insert
-            Console.WriteLine(degisken.Insert(0,"mehmet"));
-            //padleft,PadRight
-            Console.WriteLine(degisken+degisken2.PadLeft(30));
-            Console.WriteLine(degisken + degisken2.PadLeft(30,'-'));
-            Console.WriteLine(degisken.PadRight(40)+degisken2);
-            Console.WriteLine(degisken.PadRight(40,'*') + degisken2);
+            ////indexof
+            //Console.WriteLine(degisken.IndexOf("cS"));
+            //Console.WriteLine(degisken.IndexOf("mehmet"));
+            //Console.WriteLine(degisken.LastIndexOf("i"));
+            ////insert
+            //Console.WriteLine(degisken.Insert(0,"mehmet"));
+            ////padleft,PadRight
+            //Console.WriteLine(degisken+degisken2.PadLeft(30));
+            //Console.WriteLine(degisken + degisken2.PadLeft(30,'-'));
+            //Console.WriteLine(degisken.PadRight(40)+degisken2);
+            //Console.WriteLine(degisken.PadRight(40,'*') + degisken2);
 
-            //remove
-            Console.WriteLine(degisken.Remove(10));
-            Console.WriteLine(degisken.Remove(5,3));
-            Console.WriteLine(degisken.Remove(0,1));
+            ////remove
+            //Console.WriteLine(degisken.Remove(10));
+            //Console.WriteLine(degisken.Remove(5,3));
+            //Console.WriteLine(degisken.Remove(0,1));
 
-            //replace
-            Console.WriteLine(degisken.Replace("cSharp","C#"));
-            Console.WriteLine(degisken.Replace(" ","-"));
+            ////replace
+            //Console.WriteLine(degisken.Replace("cSharp","C#"));
+            //Console.WriteLine(degisken.Replace(" ","-"));
 
-            //split
-            Console.WriteLine(degisken.Split(" ")[0]);
+            ////split
+            //Console.WriteLine(degisken.Split(" ")[0]);
 
-            //substring
-            Console.WriteLine(degisken.Substring(4));
-            Console.WriteLine(degisken.Substring(4,6));
-
-
+            ////substring
+            //Console.WriteLine(degisken.Substring(4));
+            //Console.WriteLine(degisken.Substring(4,6));
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            string s;
+                int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                 s = Console.ReadLine();
+                result(s);
+            }
 
 
 
 
         }
+        static void result(string s)
+        {
+            string cift = "";
+            string tek = "";
+
+
+            for (int i = 0; i<s.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    cift += s[i];
+                }
+                else
+                {
+                    tek += s[i];
+                }
+
+
+
+            }
+            Console.WriteLine($"{cift} {tek}");
+        }
+
 
 
 
---
+++ hazırMetotlar-DatetimeVeMath/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace hazırMetotlar_DatetimeVeMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(3));
+            Console.WriteLine(DateTime.Now.AddSeconds(3));
+            Console.WriteLine(DateTime.Now.AddMonths(5));
+            Console.WriteLine(DateTime.Now.AddYears(10));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(50));
+
+            //datetime Format
+
+            Console.WriteLine(DateTime.Now.ToString("dd"));
+            Console.WriteLine(DateTime.Now.ToString("ddd"));
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+
+            Console.WriteLine(DateTime.Now.ToString("MM"));
+            Console.WriteLine(DateTime.Now.ToString("MMM"));
+            Console.WriteLine(DateTime.Now.ToString("MMMMM"));
+
+
+            //math kütüphaneesi
+            Console.WriteLine(Math.Abs(-22));//mutlak deger
+            Console.WriteLine(Math.Sin(60));
+            Console.WriteLine(Math.Ceiling(22.3));
+            Console.WriteLine(Math.Round(22.3));
+            Console.WriteLine( Math.Round(22.7));
+            Console.WriteLine(Math.Floor(22.7));
+
+
+            Console.WriteLine(Math.Max(2,6));
+            Console.WriteLine(Math.Min(2,6));
+
+            Console.WriteLine(Math.Pow(3,4));//üs alma
+            Console.WriteLine(Math.Sqrt(9));//karekok
+            Console.WriteLine(Math.Log(9));
+            Console.WriteLine(Math.Exp(3));//e üzeri
+            Console.WriteLine( Math.Log10(10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
